3ff0334ada85219309597b49debaed59671311a7
赖诗培 <88699094+CG050523@users.noreply.github.com>
2024-04-10 20:09:23 +0800

    Add files via upload

--- degw.py
+++ degw.py
@@ -1,9 +1,9 @@
 import random
-from scipy.optimize import rastrigin
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 population_size = 20
-iterations_max = 500
+iterations_max = 5000
 crossover_pro = 0.2
 # 定义搜索空间
 search_space_x = (-5, 5)
@@ -14,10 +14,16 @@ for _ in range(population_size):
     x = random.uniform(search_space_x[0], search_space_x[1])
     population.append([x])
 
+population = np.array(population)
+
 a = 2
 A = 2 * a * random.random() - a
 C = 2 * random.random()
 decrease = 2/iterations_max
+
+def rastrigin(x):
+    const = 10
+    return const * np.sum(np.square(x) - const * np.cos(2 * np.pi * x))
 
 def mutate(a,A,C):
     return a + scaling_factor * (A - C)
@@ -26,56 +32,53 @@ def loss_fuc(x):
     return rastrigin(x)
 
 def select_fuc(population, loss_fuc):
-    for i in range(population):
-        if population[i] < loss_fuc(mutate(a,A,C)):
-            population[i] = population[i]
-        if population[i]>= loss_fuc(mutate(a,A,C)):
+    for i in range(population_size):
+        if population[i] >= loss_fuc(mutate(a,A,C)):
             population[i] = mutate(a,A,C)
     return population
 
-def dis_calc(population):
-    for i in (3, population):
-        individual = population[i]
-        D_alpha = abs(C * X_alpha - individual)
-        D_beta = abs(C * X_beta - individual)
-        D_gamma = abs(C * X_beta - individual)
-        X_1 = X_alpha - A * D_alpha
-        X_2 = X_beta - A * D_beta
-        X_3 = X_gamma - A * D_gamma
-        individual = (X_1 + X_2 + X_3) / 3
-        population[i] = individual
+def crossover(population):
+    for i in range(population_size):
+        if random.random() <= crossover_pro or i == random.randint(1,population_size):
+            population[i] = mutate(a,A,C)
     return population
+
+def update(population):
+    objective_values = [loss_fuc(population[x]) for x in range(population_size)]
+    sorted_indexes = sorted(range(len(objective_values)), key = lambda i : objective_values[i])
+    sorted_population = [population[i] for i in sorted_indexes]
+    return sorted_population
 
 population = select_fuc(population, loss_fuc)
 
 t = 1
+
+result = [None] * iterations_max
+
 for t in range(iterations_max):
-    objective_values = [loss_fuc(x) for x in population]
-    sorted_indexes = sorted(range(len(objective_values)), key=lambda i : objective_values[i])
-    sorted_population = [population[i] for i in sorted_indexes]
+    sorted_population = update(population)
     best_individuals = sorted_population[:3]
     X_alpha = best_individuals[0]
     X_beta = best_individuals[1]
     X_gamma = best_individuals[2]
-    for i in (3, sorted_population):
-        individual = sorted_population[i]
-        D_alpha = abs(C * X_alpha - individual)
-        D_beta = abs(C * X_beta - individual)
-        D_gamma = abs(C * X_beta - individual)
+    # fourth_item_onwards = sorted_population[3:]
+    for i in np.arange(3, len(sorted_population)):
+        D_alpha = abs(C * X_alpha - sorted_population[i])
+        D_beta = abs(C * X_beta - sorted_population[i])
+        D_gamma = abs(C * X_beta - sorted_population[i])
         X_1 = X_alpha - A * D_alpha
         X_2 = X_beta - A * D_beta
         X_3 = X_gamma - A * D_gamma
-        individual = (X_1 + X_2 + X_3) / 3
-        sorted_population[i] = individual
+        sorted_population[i] = (X_1 + X_2 + X_3) / 3
     population = sorted_population
-    a = a - t * decrease
+    a = a - decrease
     A = 2 * a * random.random() - a
     C = 2 * random.random()
-    for j in range(population):
-        if random.random() <= crossover_pro | j  == random.randint(1,population_size):
-            population[j] = mutate(a, A, C)
-        if random.random() > crossover_pro | j !=random.randint(1,population_size):
-            population[j] = population[j]
+    population = crossover(population)
     population = select_fuc(population, loss_fuc)
+    population = update(population)
+    result[t]  = loss_fuc(population[0])
 
-print(population)
+plt.figure()
+plt.plot(result)
+plt.show()
